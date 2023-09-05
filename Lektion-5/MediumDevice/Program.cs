9de1ac8e7de96506f7bf65c9073f89a1b1bfbbd8
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-09-05 11:43:36 +0200

    Lektion 5

--- Lektion-5/MediumDevice/Program.cs
+++ Lektion-5/MediumDevice/Program.cs
@@ -1,3 +1,7 @@
 ﻿using MediumDevice.Services;
 
 var device = new DeviceManager("HostName=kyh-iothub.azure-devices.net;DeviceId=medium_device;SharedAccessKey=N3nOUoJEHyFjynGcBYAGLn6bIvxPP2OO6jmczXrvVVM=");
+await device.StartAsync();
+
+
+
--- Lektion-5/MediumDevice/Services/DeviceManager.cs
+++ Lektion-5/MediumDevice/Services/DeviceManager.cs
@@ -1,4 +1,8 @@
-﻿namespace MediumDevice.Services;
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MediumDevice.Services;
 
 internal class DeviceManager
 {
@@ -8,4 +12,31 @@ internal class DeviceManager
 	{
         Configuration = new DeviceConfig(connectionString);
 	}
+
+	public async Task StartAsync()
+	{
+        while (true)
+        {
+            var data = new
+            {
+                Temperature = 20,
+                Humidity = 44,
+                Created = DateTime.Now
+            };
+
+            await SendDataAsync(JsonConvert.SerializeObject(data));
+            await Task.Delay(Configuration.TelemetryInterval);
+        }
+    }
+
+
+	private async Task SendDataAsync(string dataAsJson)
+	{
+		if (!string.IsNullOrEmpty(dataAsJson))
+		{
+			var message = new Message(Encoding.UTF8.GetBytes(dataAsJson));
+			await Configuration.DeviceClient.SendEventAsync(message);
+		}
+	}
+
 }
