d3c729e4bbd73edd89c8458fe65b0caecfdf1ab3
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-09-08 13:27:54 +0200

    Lektion 6 (WPF - IoT Devices)

--- Lektion-6/Devices/Device.Fan/MainWindow.xaml.cs
+++ Lektion-6/Devices/Device.Fan/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,19 @@ namespace Device.Fan
         public MainWindow()
         {
             InitializeComponent();
+            Task.WhenAll(ToggleFanStateAsync());
+        }
+
+
+        private async Task ToggleFanStateAsync()
+        {
+            Storyboard fan = (Storyboard)this.FindResource("FanStoryboard");
+
+            while (true)
+            {
+                fan.Begin();
+                await Task.Delay(1000);
+            }
         }
     }
 }
