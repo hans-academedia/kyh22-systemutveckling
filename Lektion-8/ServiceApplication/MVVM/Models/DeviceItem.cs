bf2c2ca41fb87317334fc6a6ec2c83db9ec8a224
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-09-15 13:42:56 +0200

    Lektion 8 (WPF - Service Application)

---
+++ Lektion-8/ServiceApplication/MVVM/Models/DeviceItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceApplication.MVVM.Models;
+
+public class DeviceItem
+{
+    public string? DeviceId { get; set; }
+    public string? DeviceType { get; set; }
+    public string? Placement { get; set; }
+    public bool IsActive { get; set; } = false;
+    public string? Icon { get; set; } = "\uf2db";
+}
--- Lektion-8/ServiceApplication/MainWindow.xaml.cs
+++ Lektion-8/ServiceApplication/MainWindow.xaml.cs
@@ -1,36 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ServiceApplication
+namespace ServiceApplication;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+        InitializeComponent();
+      
+    }
 
-            
-        }
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
-        }
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+            this.DragMove();
     }
 }
