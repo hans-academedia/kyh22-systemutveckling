7433935af6b3fa4e3a91a10604a20466913284fb
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-09-19 12:06:13 +0200

    Lektion 9 (WPF - Navigering)

--- Lektion-9/ServiceApplication/MVVM/Core/RelayCommand.cs
+++ Lektion-9/ServiceApplication/MVVM/Core/RelayCommand.cs
@@ -9,23 +9,17 @@ public class RelayCommand : ICommand
 	private readonly Func<object, bool> _canExecute;
 	public event EventHandler? CanExecuteChanged;
 
-
 	public RelayCommand(Action execute, Func<bool> canExecute = null!)
 		: this (_ => execute(), canExecute != null ? _ => canExecute() : null!)
 	{
 
 	}
 
-
 	public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null!)
 	{
 		_execute = execute ?? throw new ArgumentException(nameof(execute));
 		_canExecute = canExecute ?? (_ => true);
 	}
-
-	
-
-
 
 	public bool CanExecute(object? parameter) => _canExecute(parameter!);
 	public void Execute(object? parameter) => _execute(parameter!);
